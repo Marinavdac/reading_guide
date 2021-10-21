05b116b0590c78c36cb22dd9deadbc6f456bfc82
Marina Andrade Catão <marinavdac@gmail.com>
2021-10-21 17:10:54 -0300

    build: build main pages routes

---
+++ src/Pages/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Books = () => {
+    return (
+        <div>
+            <h1>books and reviews
+                Click here to see
+            </h1>
+        </div>
+    );
+}
+
+export default Books;
